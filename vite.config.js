4e9f48f2a77fd85c812d7e0aadc8e51ede02ebc0
dhrxh99 <dhrxhan@gmail.com>
2025-08-20 11:50:09 +0900

    proxy 수정

--- vite.config.js
+++ vite.config.js
@@ -8,16 +8,6 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/saving-posting' : {
-        target: 'http://localhost:8091',
-        changeOrigin: true,
-      },
-      // 댓글/게시글 등 백엔드 라우트도 추가:
-      '/post': {
-        target: 'http://localhost:8091',
-        changeOrigin: true,
-      },
-      // REST로 분리돼 있다면 공통 프리픽스 묶기
       '/api': {
         target: 'http://localhost:8091',
         changeOrigin: true,
