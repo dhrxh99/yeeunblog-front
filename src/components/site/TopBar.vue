4bf0305c1734304eadb843bd9c0b8cfc16806417
dhrxh99 <dhrxhan@gmail.com>
2025-08-21 12:57:00 +0900

    홈버튼 import 이름 수정

--- src/components/site/TopBar.vue
+++ src/components/site/TopBar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-light bg-light border-bottom px-3 mb-9" style="height:100px; ">
     <RouterLink to="/" class="d-flex align-items-center" style="margin-left:150px;">
       <!-- 홈버튼 아이콘 -->
-      <img src='/src/assets/images/homeButton.png' alt="홈버튼" style="width:40px; height:40px; margin-right:8px; margin-left:1px;">
+      <img :src='homeIcon' alt="홈버튼" style="width:40px; height:40px; margin-right:8px; margin-left:1px;">
       홈버튼
     </RouterLink>
     <span class="mx-auto h4 mb-0">{{ title }}</span>
@@ -11,7 +11,7 @@
 
 <script setup>
 import { RouterLink } from 'vue-router'
-import usagiIcon from '@/assets/images/homeButton.png'
+import homeIcon from '@/assets/images/homeButton.png'
 
 const props = defineProps({
   title: { type: String, default: 'Welcome to my Blog' }
