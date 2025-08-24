32c27df336a2c8af6983d73c318065fb9a1f5ebc
dhrxh99 <dhrxhan@gmail.com>
2025-08-24 14:31:54 +0900

    게시글 상세 페이지 이동, 기타 수정

--- src/pages/CategoryPage.vue
+++ src/pages/CategoryPage.vue
@@ -18,15 +18,21 @@
     
         <div class="row g-3">
           <div class="col-md-4" v-for="post in posts" :key="post.id">
-            <div class="card h-100">
+            <RouterLink :to="`/post/${post.id}`" class="text-decoration-none text-dark">
+            <div class="card h-100 show=sm">
               <div class="card-img-top bg-light d-flex align-items-center justify-content-center" style="height:120px;">
-                <span v-if="post.thumbnail">썸네일</span>
+                <img v-if="post.thumbnail" :src="post.thumbnail" alt="썸네일" style="max-height:100%; max-width:100%; object-fit:cover;"/>
                 <span v-else>.img</span>
               </div>
+
               <div class="card-body text-center">
-                <h5 class="card-title">{{ post.title }}</h5>
+                <h5 class="card-title mb-2">{{ post.title }}</h5>
+                <p class="card-text small text-muted">
+                  {{ post.author }} · {{ post.createdAt }}
+                </p>
               </div>
             </div>
+            </RouterLink>
           </div>
         </div>
 
@@ -55,7 +61,7 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import postingIcon from '@/assets/images/postingButton.png'
 import { useRoute } from 'vue-router'
 import axios from 'axios'
@@ -94,4 +100,13 @@ function changeSort(newSort) {
 onMounted(() => {
   fetchPosts()
 })
+
+watch(
+  () => route.params.category,
+  (newCategory) => {
+    category.value = newCategory || "전체"
+    fetchPosts(1)
+  }
+)
+
 </script>
