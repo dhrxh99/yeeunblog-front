457ef7a8956b96a0cbeedcf6b1f864b60e99a605
dhrxh99 <dhrxhan@gmail.com>
2025-08-28 14:40:41 +0900

    이미지 업로드

--- src/components/site/ToastEditor.vue
+++ src/components/site/ToastEditor.vue
@@ -6,7 +6,8 @@
 <script setup>
 import '@toast-ui/editor/dist/toastui-editor.css'
 import { Editor } from '@toast-ui/editor'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
+import axios from 'axios'
 
 const props = defineProps({ modelValue: String })
 const emit = defineEmits(['update:modelValue'])
@@ -18,13 +19,36 @@ onMounted(() => {
   instance = new Editor({
     el: editorRoot.value,      // Vue div에 mount
     height: '500px',
-    initialEditType: 'markdown',
-    previewStyle: 'vertical',
-    initialValue: props.modelValue || ''
+    initialEditType: 'wysiwyg',
+    hideModeSwitch: true,
+    initialValue: props.modelValue || '',
+    hooks: {
+        addImageBlobHook: async (blob, callback) => {
+            const formData = new FormData()
+            formData.append("image", blob)
+
+            try {
+                const res = await axios.post("/api/uploads", formData, {
+                    headers: { "Content-Type": "multipart/form-data" }
+                })
+                callback(res.data.url, "image alt text")
+            } catch (e) {
+                alert("이미지 업로드 실패")
+                console.error(e)
+            }
+        }
+    }
   })
 
   instance.on('change', () => {
-    emit('update:modelValue', instance.getMarkdown())
+    emit('update:modelValue', instance.getHTML())
   })
 })
+
+watch(() => props.modelValue, (newVal) => {
+    if (instance && newVal !== instance.getHTML()) {
+        instance.setHTML(newVal || '')
+    }
+})
+
 </script>
